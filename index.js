2023a57096e0cb2df4d2b5059b677479677250be
Michal Vich <vich@zensys.cz>
2014-08-18 08:53:04 +0200

    first proof of concept how to call deezer api

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+var http = require('http');
+var request = require('request');
+var async = require('async');
+
 module.exports = {
 
-  album: function(albumId) {
-    return 'test';
+  album: function(albumId, callback) {
+
+    async.waterfall([
+
+      function(callback){
+
+        request('http://api.deezer.com/album/' + albumId, function(error, response, body) {
+          callback(null, JSON.parse(body));
+        });
+
+      }
+
+    ], function (err, result) {
+      callback(result);
+    });
+
   }
 
 }
--- test/index.js
+++ test/index.js
@@ -3,8 +3,11 @@ var deezer = require('../index');
 
 describe('#album', function() {
 
-  it('should returns album information for required album id', function() {
-    deezer.album(1).should.equal('test');
+  it('should returns album information for required album id', function(done) {
+    deezer.album(1, function(response) {
+      response.artist.name.should.equal('The Beatles');
+      done();
+    });
   });
 
 });
